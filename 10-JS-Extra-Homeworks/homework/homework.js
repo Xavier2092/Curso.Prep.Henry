bd83ee44c313352ca75ea9d9f58018ec2a056382
Xavier2092 <2092.rxcm@gmail.com>
2022-01-11 02:14:05 -0500

    Ejercicios extras

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -3,13 +3,21 @@
 function deObjetoAmatriz(objeto){
   // Escribe una función que convierta un objeto en una matriz, donde cada elemento representa 
   // un par clave-valor en forma de matriz.
-  //Ejemplo: 
+  //Ejemplo:
   /*objeto({
       D: 1,
       B: 2,
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var nuevoArreglo = [];
+  for (key in objeto){
+    nuevoArreglo.push([key, objeto[key]]);
+  }
+  //MANERA CORTA
+  //var nuevoArreglo = Object.entries(objeto);
+  return nuevoArreglo;
+
 }
 
 
@@ -18,6 +26,15 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var objeto = {};
+  for( var i = 0; i < string.length; i++ ){
+    if(Object.keys(objeto).includes(string[i])){
+      objeto[string[i]] = objeto[string[i]] + 1;
+    }else{
+      objeto[string[i]] = 1;
+    }
+  }
+  return objeto;
 }
 
 
@@ -26,6 +43,17 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var lowerCase = '';
+  var upperCase = '';
+
+  for( var i = 0; i < s.length; i++ ){
+    if( s[i] === s[i].toUpperCase() ){
+      upperCase += s[i];
+    }else{
+      lowerCase += s[i];
+    }
+  }
+  return upperCase + lowerCase;
 }
 
 
@@ -35,7 +63,11 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
-} 
+  var alReves = str.split(' ').map(function(e){
+    return  e.split('').reverse().join('');
+  }).join(' ');
+  return alReves;
+}
 
 
 function capicua(numero){
@@ -43,6 +75,9 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  numero = numero.toString();
+  var nuevoNum = numero.split('').reverse().join('');
+  return numero === nuevoNum ? 'Es capicua' : 'No es capicua';
 }
 
 
@@ -50,6 +85,13 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var newChain = '';
+  for( var i = 0; i < cadena.length; i++ ){
+    if( cadena[i] != 'a' && cadena[i] != 'b' && cadena[i] != 'c' ){
+      newChain += cadena[i];
+    }
+  }
+  return newChain;
 }
 
 
@@ -57,15 +99,28 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  var newArray = arr.sort(function(x, y) {
+    return x.length - y.length;
+  });
+  return newArray;
 }
 
 
 function buscoInterseccion(arreglo1, arreglo2){
-  //Existen dos arrays, cada uno con 5 números. A partir de ello, escribir una función que permita 
+  //Existen dos arrays, cada uno con 5 números. A partir de ello, escribir una función que permita
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
-  //Escribe tu código aquí  
+  //Escribe tu código aquí
+  var unionArray = [];
+  for( var i = 0; i < arreglo1.length; i++ ){
+    for( var j = 0; j < arreglo2.length; j++ ){
+      if( arreglo1[i] === arreglo2[j] ){
+        unionArray.push(arreglo1[i]);
+      };
+    };
+  };
+  return unionArray;
 }
 
 
